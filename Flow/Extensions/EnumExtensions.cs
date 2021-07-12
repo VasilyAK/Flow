e49f17dcf2a5e67cccd298b3f6f20e3815a8e7f6
VAK <kaluginchernyhovsck@mail.ru>
2021-07-12 21:49:00 +0200

    add Enum extensions tests

--- Flow/Extensions/EnumExtensions.cs
+++ Flow/Extensions/EnumExtensions.cs
@@ -1,9 +1,8 @@
 ﻿namespace Flow.Extensions
 {
-    //ToDo добавить тесты
     public static class EnumExtensions 
     {
         public static string FullName<TIndex>(this TIndex enumerator) where TIndex : struct
-            => typeof(TIndex).Assembly.FullName.ToString() + enumerator.ToString();
+            => $"{typeof(TIndex).FullName}.{enumerator}";
     }
 }
---
+++ FlowTests/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using Flow.Extensions;
+using FlowTests.Fakes;
+using FluentAssertions;
+using Xunit;
+
+namespace FlowTests.Extensions
+{
+    public class EnumExtensionsTests
+    {
+        [Fact]
+        public void FullName_ShouldReturnNameSpaceAndName()
+        {
+            // Act
+            var fullName = FakeNodeIndex.Index1.FullName();
+
+            // Assert
+            fullName.Should().Be("FlowTests.Fakes.FakeNodeIndex.Index1");
+        }
+    }
+}
