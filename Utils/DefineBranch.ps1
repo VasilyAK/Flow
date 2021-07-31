63bd085f0dc3d318c8c15226f3f73b19083c872f
VAK <kaluginchernyhovsck@mail.ru>
2021-07-31 19:53:17 +0200

    add release preparation

---
+++ Utils/DefineBranch.ps1
@@ -0,0 +1,15 @@
+﻿[string] $BranchHead = ${env:$GITHUB_REF}
+
+function Get-Git-BranchName {
+    param ([string] $GitBranchHead)
+    
+    [string] $GitBranchFullName = $GitBranchHead.Replace("refs/heads/", "").Trim()
+    [string[]] $GitBranchDirs = $GitBranchFullName.Split("/")
+    [string] $GitBranchName = $GitBranchDirs[0]
+
+    Write-Output $GitBranchName
+}
+
+[string] $BranchName = Get-Git-BranchName -GitBranchHead $BranchHead
+Write-Host "Branch name: $BranchName"
+${env:$BRANCH_NAME} = $BranchName
---
+++ Utils/GetNewVersion.ps1
@@ -0,0 +1,17 @@
+﻿[string] $BranchName = ${env:$BRANCH_NAME}
+[string] $ReleaseBranch = "release"
+
+function Get-ReleaseVersion {
+    param ([string] $GitBranchName)
+
+    [string] $ReleaseVersion = "no release"
+
+    if ($GitBranchName.StartsWith("$ReleaseBranch")) {
+        $ReleaseVersion = $GitBranchName.Replace("$ReleaseBranch-", "")
+    }
+
+    Write-Output $ReleaseVersion
+}
+
+[string] $ReleaseVersion = Get-ReleaseVersion -GitBranchName $BranchName
+Write-Host "Release version: $ReleaseVersion"
